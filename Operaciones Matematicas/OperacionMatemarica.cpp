a27e6bac8b50f9126ab9ddefae54f8479403db2b
Brian Enrique Marquez Inca Roca <brian3marquez@outlook.com>
2020-06-01 16:46:46 -0500

    Operacion Matemaricas

---
+++ Operaciones Matematicas/OperacionMatemarica.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ float x,y,s,r,p,d;
+ cout<<"\n ingrese numero x:";
+ cin>>x;
+ cout<<"\n ingrese numero y:";
+ cin>>y;
+ 
+ s=x+y;
+ r=x-y;
+ p=x*y;
+ d=x/y;
+ cout<<x<<"+"<<y<<"="<<s;
+ cout<<"\n"<<x<<"-"<<y<<"="<<r;
+ cout<<"\n"<<x<<"*"<<y<<"="<<p;
+ cout<<"\n"<<x<<"/"<<y<<"="<<d;
+  return 0;
+}
