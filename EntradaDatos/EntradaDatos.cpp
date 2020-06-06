91f7d9e0afae98fab01aafff45bbe6c24655aae0
Brian Enrique Marquez Inca Roca <brian3marquez@outlook.com>
2020-06-06 14:28:40 -0500

    Entrada de datos

---
+++ EntradaDatos/EntradaDatos.cpp
@@ -0,0 +1,16 @@
+/*Entrada de datos en c++*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float flotante;
+
+    cout<<"Ingrese un Numero: ";
+    cin >> flotante;
+
+    cout<<"\nEl numero es: "<<flotante;
+
+}
