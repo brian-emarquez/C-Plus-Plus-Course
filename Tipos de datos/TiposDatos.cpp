3140c6df0644621a890132d514affb521292b01a
Brian Enrique Marquez Inca Roca <brian3marquez@outlook.com>
2020-06-06 22:00:55 -0500

    Tipo de datos corregido

--- Tipos de datos/TiposDatos.cpp
+++ Tipos de datos/TiposDatos.cpp
@@ -5,6 +5,50 @@ using namespace std;
 
 int main()
 {
+ 
+    int entero = 10;
+    cout<<entero<<endl;
+    
+    float flotante = 10.5;
+    cout<<flotante<<endl;
+
+    double doublee = 1.256564654646465;
+    cout<<doublee<<endl;
+
+    char letra = 'a';
+    cout<<letra<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
 
     int entero = 10; //numero de entero
     cout<<entero<<endl;
@@ -21,3 +65,4 @@ int main()
     return 0;
 
 }
+*/
