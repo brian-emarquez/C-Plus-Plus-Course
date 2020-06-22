4d070e7b1e2a043a2d295f8110dbc39bdc9f0e8f
BrianMarquez3 <brian3marquez@gmail.com>
2020-06-21 20:32:07 -0500

    Buble For

---
+++ Bucle - For/Bucle - For/For.cpp
@@ -0,0 +1,21 @@
+/*Sentencia For*/
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+	
+	int i;
+
+	for (i = 1; i <= 10; i++) {
+		cout << i << endl;
+	}
+
+	for (int x = 1; x <= 10; x++) { /* se declara la variable y se asina su valor*/
+		cout << x << endl;
+	}
+
+	_getch();
+	return 0;
+}
