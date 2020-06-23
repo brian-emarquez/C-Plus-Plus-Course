45cca9fdce7bf8c9a8089ef8701472e7f0a5ef99
BrianMarquez3 <brian3marquez@gmail.com>
2020-06-23 17:48:54 -0500

    Graph - Interfas Grafica

---
+++ Graphic/Graphic/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+
+using namespace Graphic;
+[STAThreadAttribute]
+
+int main() {
+	MyForm mf;
+	mf.ShowDialog();
+	return 0;
+}
+
---
+++ Graphic/Graphic/MyForm.h
@@ -0,0 +1,101 @@
+#pragma once
+
+namespace Graphic {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+	private: System::Windows::Forms::ToolTip^ toolTip1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ Msg_text;
+
+
+
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->toolTip1 = (gcnew System::Windows::Forms::ToolTip(this->components));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->Msg_text = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(214, 114);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(100, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"OK";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// Msg_text
+			// 
+			this->Msg_text->Location = System::Drawing::Point(155, 77);
+			this->Msg_text->Name = L"Msg_text";
+			this->Msg_text->Size = System::Drawing::Size(227, 20);
+			this->Msg_text->TabIndex = 1;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(535, 248);
+			this->Controls->Add(this->Msg_text);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	
+	}
+	};
+}
