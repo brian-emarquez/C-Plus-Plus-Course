d51ec8c413568d24b336ac8462dde328da490786
BrianMarquez3 <brian3marquez@gmail.com>
2020-06-23 15:24:19 -0500

    Cajero Automatico Correjido

--- Cajero Automatico/Cajero Automatico/Cajero_Automatico.cpp
+++ Cajero Automatico/Cajero Automatico/Cajero_Automatico.cpp
@@ -1,39 +1,53 @@
 /*Simuladores de Cajero Automatico*/
 /*Hacer un programa que simule un cajero automatoc con un saldo inicial de 1000 Dolores*/
 
+
 #include<iostream>
+#include<cstdlib>
+
 using namespace std;
 
 int main()
 
 {
 	float saldo = 1000, cantida_mone_sum, estado_sum, cantida_mone_res, estado_res;
-	int opc;
+	int opc = 0;
 
-	cout << ".:CAJERO AUTOMATICO:."<<endl;
-	cout << "===================================" << endl;
-	cout << "1. Ver mi Estado de cuenta Actual "<< endl;
-	cout << "2. Ingresar a mi cuenta " << endl;
-	cout << "3. Hacer un Retiro " << endl;
-	cout << "4. Salir " << endl;
-	cout << "\n---Ingrese una Opcion!---" << endl;
-	cin>>opc;
 
-	
+	while (opc != 4)
+	{
+
+		cout << ".::CAJERO AUTOMATICO::." << endl;
+		cout << "===================================================" << endl;
+		cout << "1. Ver mi Estado de cuenta Actual " << endl;
+		cout << "2. Hacer deposito a tu cuenta" << endl;
+		cout << "3. Hacer un Retiro a tu cuenta" << endl;
+		cout << "4. Salir " << endl;
+		cout << "\nIngrese una Opcion!: ";
+		cin >> opc;
+
+
 		switch (opc)
 		{
-		case 1:
-			cout << "Su saldo actual es: " << saldo;
+		case 1: /*estado de cuenta*/
+			system("cls");
+			cout << "Su saldo actual es: " << saldo << "\n" << endl;
+			system("pause");
+			system("cls");
 			break;
 
-		case 2:
+		case 2: /*Deposito*/
+			system("cls");
 			cout << "Ingrese una cantidad monetaria para sumar a su cuenta" << endl;
 			cin >> cantida_mone_sum;
 			estado_sum = saldo + cantida_mone_sum;
-			cout << "Su cuenta Actual es: " << estado_sum;
+			cout << "Su cuenta Actual es: " << estado_sum << "\n" << endl;
+			system("pause");
+			system("cls");
 			break;
 
-		case 3:
+		case 3:/*Hacer un retiro*/
+			system("cls");
 			cout << "Ingrese una cantidad monetaria que desea retirar" << endl;
 			cin >> cantida_mone_res;
 			if (cantida_mone_res > saldo)
@@ -43,14 +57,24 @@ int main()
 			else
 			{
 				estado_res = saldo - cantida_mone_res;
-				cout << "Su cuenta Actual es: " << estado_res;
-				break;
-			}
+				cout << "Su cuenta Actual es: " << estado_res << "\n" << endl;
 
-		case 4:
+			}
+			system("pause");
+			system("cls");
 			break;
+
+		default:
+
+			if (opc < 1 || opc > 5) {
+				system("cls");
+				cout << "No es valido" << endl;
+				system("cls");
+
+			}
 		}
-	
-	
+	}
+	cout << "Gracias por su visita " << endl;
+
 	return 0;
 }
