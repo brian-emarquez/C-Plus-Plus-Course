99e9b2079fd995afdd434f3645274f67009a3140
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-28 22:41:29 -0500

    Array Mayor Eleento - 1

--- Array Mayor elemento/Array Mayor elemento/ArrayMayorElemento.cpp
+++ Array Mayor elemento/Array Mayor elemento/ArrayMayorElemento.cpp
@@ -1,2 +1,15 @@
 /*Escribe un programa que defina un vector de numero y muestre en la salida estandar el vector
 en orden inverso del ultimo al primer elemento*/
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+
+
+
+	_getch();
+	return 0;
+}
