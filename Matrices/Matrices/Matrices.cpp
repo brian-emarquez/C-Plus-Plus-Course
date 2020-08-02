345232be2620529888af74c9d754833f9c997b7c
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-01 22:06:09 -0500

    Introduccion a Matriz - Array Bidimencional

--- Matrices/Matrices/Matrices.cpp
+++ Matrices/Matrices/Matrices.cpp
@@ -1,1 +1,8 @@
 /*Matrices*/
+/*Array Bidimensionales - Matrices o Tablas*/
+
+/*
+¿Que es una matriz o tabla?
+Una Matriz es un vector o un tambien llamado array bidimencional. la manera
+de declarar una matriz es c++ es similar a un vector
+*/
