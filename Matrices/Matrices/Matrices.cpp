2bb6403f309ba4e5beb089d4a9001349cc85f82e
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-06 23:04:24 -0500

    Filling an array

--- Matrices/Matrices/Matrices.cpp
+++ Matrices/Matrices/Matrices.cpp
@@ -7,28 +7,39 @@ Una Matriz es un vector o un tambien llamado array bidimencional. la manera
 de declarar una matriz es c++ es similar a un vector
 */
 
-/*Rellenando una Matriz*/
+/*Rellenando una Matriz pidiendo al usuario numero de filas y columnas
+y posteriormente mostrar en lantalla*/
 
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-int main() {
 	
+int main() {
 	int numeros[100][100], filas, columnas; //100 filas y 100 columnas
 
 	cout << "Digie un numero de filas: "; cin >> filas;
 	cout << "Digie un numero de columnas: "; cin >> columnas;
 
+	//Almacenando elentos en la matriz
 	for (int i = 0; i < filas; i++) {
 		for (int j = 0; j < columnas; j++)
 		{
+			cout << "Digite un numero [" << i << "][" << j << "]: ";
+			cin >> numeros[i][j];
 
 
 		}
 	}
+	//Mostrnado elentos de a matriz
 
+	for (int i = 0; i < filas; i++) {
+		for (int j = 0; j < columnas; j++) {
+			cout << numeros[i][j];
+		}
+		cout << "\n";
+	}
 
 
 	_getch();
