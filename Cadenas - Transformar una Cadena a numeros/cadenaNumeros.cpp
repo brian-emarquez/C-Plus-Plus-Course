b14ea9ed4520e261743e2de692067f0ed0921df6
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-22 16:08:36 -0500

    Transformar una cadena a números - Función atoi() y atof() start

---
+++ Cadenas - Transformar una Cadena a numeros/cadenaNumeros.cpp
@@ -0,0 +1,22 @@
+/*************************************************/
+//Cadenas Transformar una cadena a números - Función atoi() y atof()
+/*************************************************/
+
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+
+int main() // Funcion Principal
+{
+	char palabra[] = "BRIAN";
+
+	_strlwr_s(palabra); //cambio a mayusculas
+
+	cout << palabra << endl;
+
+	_getch();
+	return 0;
+}
