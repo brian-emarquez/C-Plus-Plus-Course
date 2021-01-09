8ffdecb2342a440785c89068c9f0bc5e6f0d5b68
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-09 16:30:55 -0500

    Functions Complete

--- Funciones/funciones.cpp
+++ Funciones/funciones.cpp
@@ -1,3 +1,30 @@
 /*Define la clase LibroCalificaciones con una funcion mienbro llamada mostrar mensaje*/
 /*crea un objeto libroCAlificaciones y llama a su funcion mostrarMnesaje*/
 
+#include <iostream>
+#include <stdio.h>
+
+using std::cout;
+using std::endl;
+
+
+// definicion de las clases LibroCalificaciones
+class LibroCalificaciones
+{
+public:
+	void mostrarMensajes()
+	{
+		cout << "Bienvenido al libro de bienvenida para el usuario de libroCAlificaciones" << endl;
+	}
+};
+
+int main() 
+{
+	LibroCalificaciones miLibroCalificaciones;
+	miLibroCalificaciones.mostrarMensajes();
+	system("pause");
+	return 0;
+}
+
+
+
