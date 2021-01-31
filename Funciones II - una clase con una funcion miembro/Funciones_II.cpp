b4e8bbd545bd24549a093ee782b665771544ecb4
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-31 17:30:33 -0500

    feat Funciones II COMPLETE

--- Funciones II - una clase con una funcion miembro/Funciones_II.cpp
+++ Funciones II - una clase con una funcion miembro/Funciones_II.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
+#include <string>
 
 using namespace std;
 
+class LibroCalificaicones
+{
+public:
+	void mostarMnesaje(string nombreCurso)
+	{
+		cout << "Bienvenido al libro de calificaciones!" << nombreCurso <<endl;
+	}
+};
+
 int main() {
+	string nonbreDelCurso;
+	LibroCalificaicones miLibroCalificaicones;
 
+	//Ingreso de datos
+	cout << "Escriba el nombre del curso" << endl;
+	getline(cin, nonbreDelCurso);
+	cout << endl;
 
+	miLibroCalificaicones.mostarMnesaje(nonbreDelCurso);
 	system("pause");
 	return 0;
 }
