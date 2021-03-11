0c0a114a366278a6af7d470636f9f03d12ce86d1
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-11 18:44:31 -0500

    Ordenamiento por insercion

---
+++ Ordenamiento-porInsercion/Insercion.cpp
@@ -0,0 +1,37 @@
+/*
+Insercion
+*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+	int numeros[] = { 4,2,3,1,5 };
+	int i, pos, aux;
+
+	//Ordenamiento por insercion
+	for (i = 0; i < 5; i++) {
+		pos = i; //fecha
+		aux = numeros[i];
+
+		while ((pos > 0) && (numeros[pos - 1] > aux)) {
+			numeros[pos] = numeros[pos - 1];
+			pos--;
+		}
+		numeros[pos] = aux;
+	}
+	cout << "Orde Ascendente: ";
+	for (i = 0; i < 5; i++) {
+		cout<< numeros[i] << " ";
+	}
+	cout << "\n========================" << endl;
+	cout << "Orde descemdente: ";
+	for (i = 4; i >= 0; i--) {
+		cout << numeros[i] << " ";
+	}
+
+	_getch();
+}
