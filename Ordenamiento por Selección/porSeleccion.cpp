e3aad087e3ad9675950f7360adb8cbe7e96c3a4e
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-11 19:34:48 -0500

    Ordenamiento por selecciom

--- Ordenamiento por Selección/porSeleccion.cpp
+++ Ordenamiento por Selección/porSeleccion.cpp
@@ -9,6 +9,26 @@ using namespace std;
 
 int main() {
 
+	// Algoritmo del ordenamiento pro seleccion
+	int numeros[] = { 3,2,1,5,4 };
+	int i, j, aux, min;
+
+	for (i = 0; i < 5; i++) {
+		min = i;
+		for (j = i + 1; i < 5; i++) {
+			if (numeros[j] < numeros[min]) {
+				min = j;
+			}
+		}
+		aux = numeros[i];
+		numeros[i] = numeros[min];
+		numeros[min] = aux;
+	}
+	cout << "Orden ascentente; ";
+	for (i = 0; i < 5; i++) {
+		cout << numeros[i] << " ";
+	}
+
 
 
 	_getch();
