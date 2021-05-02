fb51cc2257f576636f617cef1e6c6a2f624422b5
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-01 19:56:21 -0500

    Public

---
+++ PublicPrivateProtected/public.cpp
@@ -0,0 +1,30 @@
+// Public
+
+#include<iostream>
+using namespace std;
+
+// class definition
+class Circle
+{
+public:
+    double radius;
+
+    double  compute_area()
+    {
+        return 3.14 * radius * radius;
+    }
+
+};
+
+// main function
+int main()
+{
+    Circle obj;
+
+    // accessing public datamember outside class
+    obj.radius = 5.5;
+
+    cout << "Radius is: " << obj.radius << "\n";
+    cout << "Area is: " << obj.compute_area();
+    return 0;
+}
