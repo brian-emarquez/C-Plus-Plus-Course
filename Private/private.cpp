20192bd70e6efa124c6750300f126bf673dbfeb7
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-01 22:02:55 -0500

    Private

--- Private/private.cpp
+++ Private/private.cpp
@@ -3,24 +3,39 @@
 #include<iostream>
 using namespace std;
 
-class Circle
-{
+class Circle{
+
+private:
+
     double radius;
 
 public:
-    double  compute_area()
-    {   
+
+   double compute_area()
+    {
         return 3.14 * radius * radius;
     }
 
+    /*Oteniendo acceso*/
+    /*setter*/
+    //void setradius(double _radius) {
+    //    radius = _radius;
+    //}
+
+    //int getradius() {
+    //    return radius;
+    //}
+
 };
+
 
 // main function
 int main()
 {
     Circle obj;
 
-    obj.radius = 1.5;
+    obj.radius(1.5);
+    /*obj.setradius (1.5);*/
 
     cout << "Area is:" << obj.compute_area();
     return 0;
