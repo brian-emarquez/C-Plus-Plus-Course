3fd7f2d7b2b308a012535ddb0131772ae69dac1f
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-02 16:56:40 -0500

    Estructuras Basicas

---
+++ Estructuras Basicas/Estructuras.cpp
@@ -0,0 +1,20 @@
+/* Estrucutras Basica*/
+
+#include<iostream>
+#include<stdio.h>
+#include "Header.h"
+
+using namespace std;
+
+int main() {
+
+	cout << "Nombre: " << persona1.nombre << endl;
+	cout << "Edad: " << persona1.edad << endl;
+
+	cout << "\nNombre2: " << persona2.nombre << endl;
+	cout << "Edad2: " << persona2.edad << endl;
+
+	system("pause");
+	return 0;
+}
+
---
+++ Estructuras Basicas/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<iostream>
+
+using namespace std;
+
+struct Persona {
+	char nombre[20];
+	int edad;
+}
+
+persona1 = { "Maria", 25 },
+persona2 = { "Mario", 30 };
