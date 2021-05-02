595233fee49e65c081e2e999babe17ea2577d821
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-02 15:31:01 -0500

    Update Archives

--- Private/Header.h
+++ Private/Header.h
@@ -1,1 +1,28 @@
 #pragma once
+#include<iostream>
+#include "Header.h"
+
+class Circle {
+
+private:
+
+    double radius;
+
+public:
+
+    double compute_area()
+    {
+        return 3.14 * radius * radius;
+    }
+
+    /*Oteniendo acceso*/
+    /*setter*/
+    //void setradius(double _radius) {
+    //    radius = _radius;
+    //}
+
+    //int getradius() {
+    //    return radius;
+    //}
+
+};
--- Private/private.cpp
+++ Private/private.cpp
@@ -1,32 +1,9 @@
 // private
 
 #include<iostream>
+#include "Header.h"
 using namespace std;
 
-class Circle{
-
-private:
-
-    double radius;
-
-public:
-
-   double compute_area()
-    {
-        return 3.14 * radius * radius;
-    }
-
-    /*Oteniendo acceso*/
-    /*setter*/
-    //void setradius(double _radius) {
-    //    radius = _radius;
-    //}
-
-    //int getradius() {
-    //    return radius;
-    //}
-
-};
 
 
 // main function
