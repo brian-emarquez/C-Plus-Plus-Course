774be6e867113318a99b1b009884a24b205f3ee8
BrianMarquez3 <brian3marquez@gmail.com>
2021-07-11 16:18:41 -0500

    Parametros por referencia

--- Funciones2 - ParametrosPorReferencia/Funciones2 - ParametrosPorReferencia/Source.cpp
+++ Funciones2 - ParametrosPorReferencia/Funciones2 - ParametrosPorReferencia/Source.cpp
@@ -1,2 +1,26 @@
 /*Paso de Parametros por referencias*/
 
+# include <iostream>
+# include <conio.h>
+
+using namespace std;
+
+void valNuevo(int&,int&);
+
+int main() {
+
+	int num1, num2;
+	cin >> num1 >> num2;
+
+	valNuevo(num1, num2);
+
+
+	_getch();
+	return 0;
+}
+
+//Pso por referencia - Drireccion
+void valNuevo(int& xnum, int& ynum) {
+	cout << "El valor del primero numer es: " << xnum << endl;
+	cout << "El valor del segundo numer es: " << ynum << endl;
+}
