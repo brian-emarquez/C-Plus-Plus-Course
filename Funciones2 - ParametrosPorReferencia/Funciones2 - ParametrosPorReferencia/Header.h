679f8a9ccad8cf3e410dd39b7e9a493396ea2993
BrianMarquez3 <brian3marquez@gmail.com>
2021-07-11 16:38:10 -0500

    Parametros por referencia

---
+++ Funciones2 - ParametrosPorReferencia/Funciones2 - ParametrosPorReferencia/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+//Pso por referencia - Drireccion
+void valNuevo(int& xnum, int& ynum) {
+	cout << "El valor del primero numer es: " << xnum << endl;
+	cout << "El valor del segundo numer es: " << ynum << endl;
+}
--- Funciones2 - ParametrosPorReferencia/Funciones2 - ParametrosPorReferencia/Source.cpp
+++ Funciones2 - ParametrosPorReferencia/Funciones2 - ParametrosPorReferencia/Source.cpp
@@ -2,6 +2,7 @@
 
 # include <iostream>
 # include <conio.h>
+# include"Header.h"
 
 using namespace std;
 
@@ -16,10 +17,4 @@ int main() {
 
 	_getch();
 	return 0;
-}
-
-//Pso por referencia - Drireccion
-void valNuevo(int& xnum, int& ynum) {
-	cout << "El valor del primero numer es: " << xnum << endl;
-	cout << "El valor del segundo numer es: " << ynum << endl;
 }
