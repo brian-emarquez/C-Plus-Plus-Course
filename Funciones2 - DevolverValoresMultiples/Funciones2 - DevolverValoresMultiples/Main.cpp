1e57bb86ecbc5ca78121fd2017257b85fc5fe219
BrianMarquez3 <brian3marquez@gmail.com>
2021-07-11 17:05:59 -0500

    Devolver Valores Multiples

--- Funciones2 - DevolverValoresMultiples/Funciones2 - DevolverValoresMultiples/Main.cpp
+++ Funciones2 - DevolverValoresMultiples/Funciones2 - DevolverValoresMultiples/Main.cpp
@@ -5,10 +5,25 @@
 
 using namespace std;
 
+void calcular(int, int, int&, int&);
+
 int main() {
 
+	int num1,num2,suma,producto;
 
+	cout << "Digite 2 numeros: ";
+	cin >> num1>> num2;
+
+	calcular(num1,num2,suma,producto);
+
+	cout << "El valor de la suma es: " << suma << endl;
+	cout << "El valor del producto es: " << producto << endl;
 
 	_getch();
 	return 0;
+}
+
+void calcular(int num1, int num2, int& suma, int& producto) {
+	suma = num1 + num2;
+	producto = num1 * num2;
 }
