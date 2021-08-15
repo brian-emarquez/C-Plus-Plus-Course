8d3e694b2ddd04f4ec193c81922239fd2c063f3c
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-15 15:58:05 -0500

    Funcionen - Recursividad Suma

---
+++ Funciones2 RecursividadSuma/Source.cpp
@@ -0,0 +1,33 @@
+/*
+
+
+
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+//prototipo
+int factorial(int);
+
+int main() {
+
+	cout << "El factorial: " << factorial(5) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int factorial(int n) {
+	if (n == 0) { //caso base
+
+		n = 1;
+	}
+	else { // caso genral
+		n = n * factorial(n - 1);
+	}
+	return n;
+}
