41d02d436d64b1aa2cfe53c98e40213a0a7cc75a
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-15 17:45:24 -0500

    Puntero - Par e Impar

---
+++ Puntero - Numero Par e Impar/punteroParImpar.cpp
@@ -0,0 +1,25 @@
+/*
+
+Comprobar si un numero es par o impar y señalar las posiciones de memoria 
+donde esta guadardo el numero con punteros
+
+*/
+
+# include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+
+
+int main() {
+
+
+
+	_getch();
+	return 0;
+
+}
+
+
