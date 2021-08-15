bdcaad5f0632629196b502f282143542f2e8e1c6
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-15 15:31:31 -0500

    Funciones con recursividad

---
+++ Funciones2 - Recursividad/Source.cpp
@@ -0,0 +1,18 @@
+/* Recursividad , es aquella funcion que se llama asi misma
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+//prototipo
+int factorial(int);
+
+int main() {
+
+
+
+	system("pause");
+	return 0;
+}
