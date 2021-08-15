312077f97aec1d7bcd175565257c1f6fb1e00993
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-15 17:55:13 -0500

    Puntero - Par e Impar

--- Puntero - Numero Par e Impar/punteroParImpar.cpp
+++ Puntero - Numero Par e Impar/punteroParImpar.cpp
@@ -1,6 +1,6 @@
 /*
 
-Comprobar si un numero es par o impar y señalar las posiciones de memoria 
+Comprobar si un numero es par o impar y señalar las posiciones de memoria
 donde esta guadardo el numero con punteros
 
 */
@@ -8,18 +8,25 @@ donde esta guadardo el numero con punteros
 # include <iostream>
 #include <conio.h>
 
-
 using namespace std;
-
-
 
 int main() {
 
+	int numero, *dir_numero;
 
+	cout << "Digite un Numero: "; cin >> numero;
+
+	dir_numero = &numero; // Guardando la posicion de memoria
+
+	if (*dir_numero % 2 == 0) {
+		cout << "El numero " << *dir_numero << " es par " << endl;
+		cout << "Posicion " << dir_numero << endl;
+	}
+	else {
+		cout << "El numero " << *dir_numero << " es impar " << endl;
+		cout << "Posicion " << dir_numero << endl;
+	}
 
 	_getch();
 	return 0;
-
 }
-
-
