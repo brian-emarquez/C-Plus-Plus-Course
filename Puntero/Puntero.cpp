cb3da94f5e465fcb4e2dd973c92a5efb9da38b58
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-15 17:33:35 -0500

    Punteros

--- Puntero/Puntero.cpp
+++ Puntero/Puntero.cpp
@@ -10,11 +10,19 @@ using namespace std;
 int main()
 {
 	int num;
-	num = 25;
-	cout << "Numero: " << num << endl;
-	//posicion de memoria
+	int *dir_num; //puntero que guarda la direccion de memoria de una variable entera
 
-	cout <<"Direccion de memoria: " <<&num << endl;
+	num = 25;
+	dir_num = &num;
+
+	cout << "Numero: " << num << endl;
+	cout << "Numero: " << *dir_num << endl;
+
+	cout << " =========================.:Memoria:.=============================" << endl;
+
+	//posicion de memoria
+	cout << "Direccion de memoria: " << &num << endl;
+	cout <<"Direccion de memoria: " << dir_num << endl;
 
 	system("pause");
 
