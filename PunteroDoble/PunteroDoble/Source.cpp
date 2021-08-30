a295b7a8407e452c4147b7e44ed709837277f0f4
BrianMarquez3 <brian3marquez@gmail.com>
2021-08-29 22:37:09 -0500

    Punteros Update

--- PunteroDoble/PunteroDoble/Source.cpp
+++ PunteroDoble/PunteroDoble/Source.cpp
@@ -7,8 +7,10 @@ int main() {
 
 	int x, y;
 	int *p, * q;
+
 	int **t;
 	int ***w;
+	int *** o;
 
 	x = 5;
 	y = 6;
@@ -16,19 +18,20 @@ int main() {
 	p = &x;
 	q = &y;
 
-	cout << *p << endl;
-	cout << *q << endl;
+	/*mostrar por pantalla*/
+	cout << "el puntero de *p es :"<< *p << endl;
+	cout << "el puntero de *q es :"<< *q << endl;
 
 	t = &p;
-	//*t = &y;
+	*t = &y;
 
 	w = &t;
+	**w = &t;
 	
-	cout << **t << endl;
-	//cout << *p << endl;
-
-	cout << ***w << endl;
-
+	cout << "***********************\n" << endl;
+	cout << "Doble puntero de **t es: "<< **t << endl;
+	cout << "triple puntero de ***w es: " <<***w << endl;
+	//cout << "cuadriple puntero de ****o es: " << ***w << endl;
 
 	cout<< "\n" << endl;
 	system("pause");
